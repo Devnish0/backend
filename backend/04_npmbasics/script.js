0c7bd0054a35b9d552abe7329e394190b1a1bc15
Your Name <your.email@example.com>
2025-10-15 01:17:19 +0530

    npm basics

--- backend/04_npmbasics/script.js
+++ backend/04_npmbasics/script.js
@@ -1,4 +1,8 @@
+const chalk = require("chalk");
+console.log(chalk.blue("Hello world!"));
+
 //! NPM understanding
+
 //? npm - node pakage manager (node apps )
 //* node pakage manager (initially only node application) ==> later more than only node apps (changed to npm)
 //* ? codebases downloader knowns as packages
@@ -16,7 +20,21 @@
 //* view all installed packages => npm list
 //* instaling particular version => npm i accessibility@0.2.1
 
+//------------------------------------------------//
+
 // understanding node modules
-// dependency
-// devdependency
+
+//------------------------------------------------//
+
+// dependency - pakages and pakages ki dependencies
+
+//------------------------------------------------//
+
+// devdependency - used during development only not when the actual app is developed example -nodemon, eslint, jest, mocha, typescript
+//? example - npm install nodemon --save-dev
+
+//------------------------------------------------//
+
 // scripts - understanding default scripts PATH and custom  scripts
+
+console.log("script ran");
